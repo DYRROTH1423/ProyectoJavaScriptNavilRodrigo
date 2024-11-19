deec842353466c298f6188d8f9c7fe978cb2ef60
matylaity <torriconathy@gmail.com>
2024-11-19 14:33:27 -0400

    RODRIGO

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+function sumar(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno+numeroDos+numeroTres;
+    alert("la resta es"+resultado);
+}
+function multiplicacion(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno*numeroDos*numeroTres;
+    alert("la multiplicacion es"+resultado);
+}
+function resta(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno-numeroDos-numeroTres;
+    alert("la resta es"+resultado);
+}
+function dividir(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno/numeroDos/numeroTres;
+    alert("la dividir es"+resultado);
+}
